7c15b701fa53247ec7d263b56fccf884cb9b6659
victor <victor@victors-MacBook-Air.local>
2023-03-23 20:24:42 -0400

    working on the home.html

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('', views.add_widget, name='add_widget'),
+    path('add_widget/', views.add_widget, name='add_widget'),
+    # path('', views.WidgetCreate.as_view(), name='widgets_create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -8,15 +8,19 @@ from .forms import WidgetForm
 
 def home(request):
      widget_form = WidgetForm()
-     
-    
+     widgets = Widget.objects.all()
+     print(widgets)
+     print(widgets.__len__())
 
-     return render(request, 'home.html',{'widget_form': widget_form})
+
+     return render(request, 'home.html',{'widget_form': widget_form, 'widgets': widgets})
 
 
 
 
 def add_widget(request):
+     print('this is the request')
+     print(request.POST)
      if request.method == 'POST':
         form = WidgetForm(request.POST)
         if form.is_valid():
@@ -24,17 +28,7 @@ def add_widget(request):
             new_widget.save()
      else:
         form = WidgetForm()
-     return redirect('home', {'form': form})  
+     return redirect('home')  
              
                 
 
-# def add_widget(request, widget_id):
-#      form = WidgetForm(request.POST)
-#      if form.is_valid():
-#           new_widget = form.save(commit=False)
-#           new_widget.save()
-#      return redirect('home', widget_id=widget_id)     
-            
-          
-
-#     #  {'widget':widget, 'widget_form': widget_form}
